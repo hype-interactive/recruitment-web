15c170512beaf42bef298be6e440093883424128
PatrickMamsery <pkrobert1612@gmail.com>
2022-10-28 11:52:18 +0300

    fix: remove validator error

--- app/Http/Controllers/Admin/BlogPostController.php
+++ app/Http/Controllers/Admin/BlogPostController.php
@@ -10,6 +10,8 @@ use Illuminate\Support\Facades\Storage;
 use Spatie\ImageOptimizer\OptimizerChainFactory;
 // use Spatie\LaravelImageOptimizer\Facades\ImageOptimizer as FacadesImageOptimizer;
 
+use Illuminate\Support\Facades\Validator;
+
 class BlogPostController extends Controller
 {
     public function show()
@@ -32,7 +34,7 @@ class BlogPostController extends Controller
     {
         $validator=Validator::make($request->all(),[
 
-            'image' => 'required|image|mimes:jpg,png,jpeg',
+            'image' => 'required|image|mimes:jpg,png,jpeg,image/jpeg,image/png',
         ]);
 
         if($validator->fails()) return back()->with('msg','Entered image of invalid type. Try again');
